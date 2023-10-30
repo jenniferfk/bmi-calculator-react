a423a1929dc666384c154242e04a02e261eac85d
jenniferfk <jenny.kamel002@gmail.com>
2023-10-30 21:06:31 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from "react";
+
+    const DEFAULT_WEIGHT = 50;
+    const DEFAULT_HEIGHT = 150;
 
 function App() {
+  const[weight,setWeight]=useState(DEFAULT_WEIGHT);
+  const[height,setHeight]=useState(DEFAULT_HEIGHT);
+  const [bmiResult, setBmiResult] = useState(null);
+
+  function onHeightChange(event){
+    const inputHeight= event.target.value;
+    setHeight(inputHeight);
+  } 
+  function onWeightChange(event){
+    const inputWeight= event.target.value;
+    setWeight(inputWeight);
+  }
+  function calcBMI(){
+    const convheight = height / 100;
+    const bmi = weight / (convheight * convheight);
+    setBmiResult(bmi);
+  }
+  useEffect(() => {
+    calcBMI();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>BMI CALCULATOR</h1>
+      <div className="maindiv">
+      <p>Weight: {weight} kg</p>
+        <input
+          onChange={onWeightChange}
+          type="range"
+          step="1"
+          min="40"
+          max="220"
+        />
+        <p>Height: {height} cm</p>
+        <input
+          onChange={onHeightChange}
+          type="range"
+          min="140"
+          max="220"
+        />
+      <div className="resultdiv">
+      <p>Your BMI IS: </p>
+      <p className="result">{bmiResult !== null ? bmiResult.toFixed(2) : ""}</p> 
+      </div>
+    </div>
     </div>
   );
 }
